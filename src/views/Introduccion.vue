ae11ea07fe0cdc487889d24f63fad2a1a5eb3760
PABG1990 <pedro19908454@gmail.com>
2025-08-26 17:23:13 -0500

    Ad_Video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -24,8 +24,8 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-        figcaption Video. 
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/OhZVUrYWcQE?si=jtL_3-AvpieoSsbl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        figcaption Video. Fundamentos técnicos y normativos del sector farmacéutico
 
 </template>
 
